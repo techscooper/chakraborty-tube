146830d5c7c8268d6122db050a320ac956cd9169
Debjyoti Biswas <dbiswas@techscooper.in>
2025-12-16 13:32:01 +0530

    Fix admin logout to use correct user ID variable
    Replaces usage of $id1_admin with $idadmin in SQL queries to ensure the correct admin user is referenced during logout.

--- logout.php
+++ logout.php
@@ -2,12 +2,12 @@
 include 'config.php';
 if ($ckadmin == '1') {
 	$llg_out = date('Y-m-d H:i:s', time());
-	$get = mysqli_query($conn,"SELECT * FROM `login_tbl` where `u_id`='$id1_admin'");
+	$get = mysqli_query($conn,"SELECT * FROM `login_tbl` where `u_id`='$idadmin'");
 	if($row=mysqli_fetch_array($get)){
 		$llg = $row['llg_in'];
 	}
 	mysqli_query($conn,"UPDATE `login_api_keys` SET `stat`='0' WHERE `session_id`='$sessionIDGet'");
-	mysqli_query($conn,"UPDATE `login_tbl` SET `llg_out`='$llg_out', `llg_in_time`='$llg' WHERE `u_id`='$id1_admin'");
+	mysqli_query($conn,"UPDATE `login_tbl` SET `llg_out`='$llg_out', `llg_in_time`='$llg' WHERE `u_id`='$idadmin'");
 }
 setcookie( "apiTokenKey", "", time()-3600, "/", $urlLink, isset($_SERVER["HTTPS"]), true);
 session_unset();
